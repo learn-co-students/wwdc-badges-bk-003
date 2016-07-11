e2b5fc8218611458b10a4e2ff2b21735fbd9082a
Brian Mesa <bmesa620@gmail.com>
2016-07-10 23:57:47 -0400

    done

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -13,6 +13,11 @@
      * Write your solution here.
      
      */
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for(NSUInteger i = 0; i < [conferenceSpeakers count]; i++){
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+    }
     
     // do not alter
     return YES;  //
